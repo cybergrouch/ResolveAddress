34c9cd17f583e1497425a074b85b3bf3843c4451
Lange Pantoja <wpantoja@paypal.com>
2015-02-03 15:29:41 +0800

    Working example of Android code that takes in input from user for address then calling Google Map Endpoint to resolve the address and showing the components of the address back to the user.

--- app/src/main/java/com/paypal/first/resolveaddress/MainActivity.java
+++ app/src/main/java/com/paypal/first/resolveaddress/MainActivity.java
@@ -8,6 +8,23 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -46,20 +63,66 @@ public class MainActivity extends ActionBarActivity {
     public void doResolveAddress(View v) {
         EditText input = (EditText) findViewById(R.id.addressText);
         Log.i(TAG, input.getText().toString());
+        HttpAsyncTask asyncTask = new HttpAsyncTask();
+        asyncTask.execute(input.getText().toString());
     }
 
     class HttpAsyncTask extends AsyncTask<String, Void, String> {
 
+        private static final String REQUEST_FORMAT = "http://maps.google.com.sg/maps/api/geocode/json?address=%s&sensor=false";
+
         @Override
         protected String doInBackground(String... params) {
             // write code to do network call
-            return null;
+            StringBuffer response = new StringBuffer();
+
+            for (String place: params) {
+                DefaultHttpClient client = new DefaultHttpClient();
+                HttpGet httpGet = null;
+                try {
+                    httpGet = new HttpGet(String.format(REQUEST_FORMAT, URLEncoder.encode(place, "UTF-8")));
+                } catch (UnsupportedEncodingException e) {
+                    Log.e(TAG, "Error encoding URL parameter");
+                }
+
+                try {
+                    HttpResponse execute = client.execute(httpGet);
+                    InputStream content = execute.getEntity().getContent();
+
+                    BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                    String s = "";
+                    while ((s = buffer.readLine()) != null) {
+                        response.append(s);
+                    }
+                } catch (Exception e) {
+                    Log.e(TAG, "Error on executing remote call", e);
+                }
+            }
+
+            return response.toString();
         }
 
         @Override
         protected void onPostExecute(String s) {
             // will run on user interface thread
             super.onPostExecute(s);
+
+            StringBuffer addressBuffer = new StringBuffer();
+
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                JSONArray resArray = (JSONArray) jsonObject.get("results");
+                JSONArray addresses = (JSONArray) ((JSONObject) resArray.get(0)).get("address_components");
+                for (int i = 0; i < addresses.length(); i++) {
+                    addressBuffer.append(((JSONObject) addresses.get(i)).getString("long_name")).append("\n");
+                }
+            } catch (Exception e) {
+                Log.e(TAG, "Error parsing result", e);
+            }
+
+            TextView view = (TextView) findViewById(R.id.resultView);
+            view.setText(addressBuffer.toString());
         }
+
     }
 }
